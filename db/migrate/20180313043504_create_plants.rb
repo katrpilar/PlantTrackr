04c141a241daa8eb15446090945a0f704d7e735e
Katrina Brinson <kbrinso@gmail.com>
2018-03-13 05:17:29 +0000

    starting to write migration tables

--- db/migrate/20180313043504_create_plants.rb
+++ db/migrate/20180313043504_create_plants.rb
@@ -1,4 +1,10 @@
 class CreatePlants < ActiveRecord::Migration[5.1]
   def change
+    create_table :plants do |t|
+      t.string :name
+      t.string :picture
+      t.integer :owner_id
+      
+    end
   end
 end
