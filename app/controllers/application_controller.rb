efae920f0909db556198ec18a645964412b6a8e2
Katrina Brinson <kbrinso@gmail.com>
2018-03-14 03:04:11 +0000

    Added cards styling for displaying plants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,36 @@ class ApplicationController < Sinatra::Base
    erb :index
   end
 
+  get '/plants' do
+    @user = User.find_by_id(session[:user_id])
+    @plants = Plant.all
+    erb :'/plants/plants'
+  end
+
   get '/signup' do
    erb :'/users/create_user'
   end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password_digest: params[:password])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect to '/plants'
+      else
+        redirect to '/signup'
+      end
+    end
+  end
+
+  get '/plants/new' do
+    erb :'/plants/create_plant'
+  end
+
+  # post '/plants' do
+  #   erb :'/plants'
+  # end
 
 end
