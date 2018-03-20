3a14fb611c39fa8fe533e5a30297c2ee235e007e
Katrina Brinson <kbrinso@gmail.com>
2018-03-20 14:30:41 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,19 @@ class ApplicationController < Sinatra::Base
   get '/plants' do
     @user = User.find_by_id(session[:user_id])
     @plants = Plant.all
-    erb :'/plants/plants'
+    if logged_in?
+      erb :'/plants/plants'
+    else
+      redirect :'/login'
+    end
   end
 
   get '/signup' do
-   erb :'/users/create_user'
+    if logged_in?
+      redirect '/plants'
+    else
+      erb :'/users/create_user'
+    end
   end
 
   post '/signup' do
@@ -38,16 +46,20 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/plants/new' do
-    erb :'/plants/create_plant'
+    if logged_in?
+      erb :'/plants/create_plant'
+    else
+      redirect :'/login'
+    end
   end
 
-  get '/admin' do
-    @users = User.all
-    erb :'/users/all_users'
-  end
+  # get '/admin' do
+  #   @users = User.all
+  #   erb :'/users/all_users'
+  # end
 
   get '/login' do
-    if session.include?(:user_id)
+    if logged_in?
       redirect '/plants'
     else
       erb :'/users/login'
@@ -65,7 +77,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/logout' do
-    if session.include?(:user_id)
+    if logged_in?
       session.clear
       redirect "/login"
     else
@@ -74,6 +86,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/plants' do
+    if logged_in?
       @plant = Plant.create(name: params[:plant][:name], picture: params[:plant][:picture], sunlight: params[:plant][:sunlight], soil: params[:plant][:soil], container_size: params[:plant][:container], drainage: params[:plant][:drainage])
       @plant.user = User.find_by_id(session[:user_id])
       @plant.save
@@ -90,6 +103,9 @@ class ApplicationController < Sinatra::Base
       else
         redirect to :'/plants/new'
       end
+    else
+      redirect to '/login'
+    end
   end
 
   post '/plants/:id/copy' do
@@ -105,19 +121,26 @@ class ApplicationController < Sinatra::Base
 
   get '/plants/:id' do
     @plant = Plant.find_by_id(params[:id])
-    # binding.pry
-    @all_statuses = @plant.statuses
-    erb :'/plants/show_plant'
+    if @plant.user == current_user
+      @all_statuses = @plant.statuses
+      erb :'/plants/show_plant'
+    else
+      redirect :'/plants'
+    end
   end
 
   get '/plants/:id/edit' do
     @plant = Plant.find_by_id(params[:id])
-    erb :'/plants/edit_plant'
+    if current_user == @plant.user
+      erb :'/plants/edit_plant'
+    else
+      redirect :'/plants'
+    end
   end
 
   delete '/plants/:id/delete' do
     @plant = Plant.find_by_id(params[:id])
-    if session[:user_id] == @plant.user_id
+    if current_user == @plant.user
       @plant.delete
       redirect to :'/plants'
     else
@@ -126,41 +149,64 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/greenhouse' do
-    @user = User.find_by_id(session[:user_id])
-    @plants = User.find_by_id(session[:user_id]).plants
-    erb :'/users/my_plants'
+    if logged_in?
+      @user = User.find_by_id(session[:user_id])
+      @plants = User.find_by_id(session[:user_id]).plants
+      erb :'/users/my_plants'
+    else
+      redirect to :'/login'
+    end
   end
 
   patch '/plants/:id' do
       @plant = Plant.find_by_id(params[:id])
-      @plant.update(name: params[:plant][:name], picture: params[:plant][:picture], sunlight: params[:plant][:sunlight], soil: params[:plant][:soil], container_size: params[:plant][:container], drainage: params[:plant][:drainage])
-      @plant.save
-      if params[:plant][:instructions][:water_amt] != @plant.instructions.last.water_amt || params[:plant][:instructions][:water_amt_unit] != @plant.instructions.last.water_amt_unit || params[:plant][:instructions][:water_freq] != @plant.instructions.last.water_freq || params[:plant][:instructions][:water_freq_unit] != @plant.instructions.last.water_freq
-        @instruction = Instruction.create(water_amt: params[:plant][:instructions][:water_amt], water_amt_unit: params[:plant][:instructions][:water_amt_unit], water_freq: params[:plant][:instructions][:water_freq], water_freq_unit: params[:plant][:instructions][:water_freq_unit])
-        @instruction.plant = @plant
-        @instruction.save
+      if current_user == @plant.user
+        @plant.update(name: params[:plant][:name], picture: params[:plant][:picture], sunlight: params[:plant][:sunlight], soil: params[:plant][:soil], container_size: params[:plant][:container], drainage: params[:plant][:drainage])
+        @plant.savegi
+        if params[:plant][:instructions][:water_amt] != @plant.instructions.last.water_amt || params[:plant][:instructions][:water_amt_unit] != @plant.instructions.last.water_amt_unit || params[:plant][:instructions][:water_freq] != @plant.instructions.last.water_freq || params[:plant][:instructions][:water_freq_unit] != @plant.instructions.last.water_freq
+          @instruction = Instruction.create(water_amt: params[:plant][:instructions][:water_amt], water_amt_unit: params[:plant][:instructions][:water_amt_unit], water_freq: params[:plant][:instructions][:water_freq], water_freq_unit: params[:plant][:instructions][:water_freq_unit])
+          @instruction.plant = @plant
+          @instruction.save
+        end
+        redirect to :"/plants/#{@plant.id}"
+      else
+        redirect to :'/plants'
       end
-      redirect to :"/plants/#{@plant.id}"
   end
 
   post '/:plant_id/statuses' do
     @plant = Plant.find_by_id(params[:plant_id])
-    @status = Status.create(event: params[:plant][:statuses][:event], event_date: params[:plant][:statuses][:event_date], soil_status: params[:plant][:statuses][:soil_status], leaf_status: params[:plant][:statuses][:leaf_status])
-    @status.plant = @plant
-    @status.save
-    redirect to :"/plants/#{@plant.id}"
+    if current_user == @plant.user
+      @status = Status.create(event: params[:plant][:statuses][:event], event_date: params[:plant][:statuses][:event_date], soil_status: params[:plant][:statuses][:soil_status], leaf_status: params[:plant][:statuses][:leaf_status])
+      @status.plant = @plant
+      @status.save
+      redirect to :"/plants/#{@plant.id}"
+    else
+      redirect to :'/plants'
+    end
   end
 
 
   delete '/plants/statuses/:stat_id/delete' do
     @status = Status.find_by_id(params[:stat_id])
     @plant = Plant.find_by_id(@status.plant_id)
-    if session[:user_id] == @plant.user_id
+    if current_user == @plant.user
       @status.delete
       redirect to :"/plants/#{@plant.id}"
     else
       redirect to :'/plants'
     end
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
   end
 
 end
